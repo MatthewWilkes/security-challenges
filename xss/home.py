55d94c5f3813930d397600065297333a4941525b
Matthew Wilkes <git@matthewwilkes.name>
2013-10-22 13:44:45 -0700

    Link to code

--- xss/home.py
+++ xss/home.py
@@ -6,5 +6,5 @@ class HomeView(Layouts):
 
     @view_config(route_name='home', renderer="templates/home.pt")
     def home(self):
-        return {'challenges': (1,2, )}
+        return {'challenges': {1:'one.py', 2:'two.py', }}
     
